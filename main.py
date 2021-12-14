3190a8b2dea2ad6b3c4a97af7bfcafb1d2afd1bd
eric mullally <ericmullally@yahoo.com>
2021-12-14 11:03:41 -0600

    a * functioning properly. need to implement start and end change capability and obstical adding

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         super(Main, self).__init__()
         self.setupUi(self)
         self.start = (0,0)
-        self.end = (15,15)
-        self.nodes = self.makeGrid(16,16)
+        self.end = (27,10)
+        self.nodes = self.makeGrid(30,30)
         self.setStartAndEnd(self.start, self.end)
         self.setNeighbors()
         self.Astar(self.start, self.end )
@@ -29,18 +29,11 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
                 node = Node(i,j)
                 newButton = QtWidgets.QPushButton("X")
                 newButton.setStyleSheet("color:white;")
-                # newButton.clicked.connect(self.buttonClick)
                 self.mainGrid.addWidget(newButton, node.position[0],node.position[1] )
                 innerList.append(node)
             grid.append(innerList)
         return grid
 
-    # def buttonClick(event):
-    #     print(event.sender())
-    #     modifiers = QtGui.QGuiApplication.keyboardModifiers()
-    #     if modifiers == QtCore.Qt.ShiftModifier:
-    #         event.sender().setText("S")
-        
     def setStartAndEnd(self, start : tuple, end: tuple):
         startButton = self.mainGrid.itemAtPosition(start[0], start[1]).widget()
         endButton = self.mainGrid.itemAtPosition(end[0], end[1]).widget()
@@ -73,17 +66,26 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
             notTestedNodes.sort(key= lambda x: x.dGlobal, reverse=True)
             currentNode = notTestedNodes.pop()
 
+            if(currentNode is endNode):
+                break
+
             if(not currentNode.visited):
-               currentNode.visited = True
+               currentNode.visited = True 
+
+               if(not currentNode is startNode): 
+                    btn = self.mainGrid.itemAtPosition(currentNode.position[0], currentNode.position[1])
+                    btn.widget().setStyleSheet("background-color:yellow;")
+
                for i in currentNode.neighbors:
-                    if(not i.visited):
+                    if(not i.visited and not i in notTestedNodes):
                        notTestedNodes.append(i)
-                
+
                     estmimattedLocalGoal = currentNode.dLocal + self.getDistance(currentNode, i)
                     if(estmimattedLocalGoal < i.dLocal):
                         i.dLocal = estmimattedLocalGoal
                         i.parent = currentNode
                         i.dGlobal = i.dLocal + self.getDistance(i, endNode) 
+                    
 
         self.showPath(endNode)     
            
@@ -92,7 +94,7 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         currentBtn = self.mainGrid.itemAtPosition(currentNode.position[0], currentNode.position[1]) 
         while currentNode.parent != None:
             if(not currentNode is endNode):
-                currentBtn.widget().setText("-")
+                currentBtn.widget().setText("")
                 currentBtn.widget().setStyleSheet("background-color: blue;")
             currentNode = currentNode.parent
             currentBtn = self.mainGrid.itemAtPosition(currentNode.position[0], currentNode.position[1]) 
@@ -103,18 +105,21 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         if the node is on an edge it sets those neighbors to none 
         then filters them out of the final list
         """
+        
         for i in range(0, len(self.nodes)):
+            maxJ = len(self.nodes[i]) -1
+            maxI = len(self.nodes) -1
+
             for j in range(0, len(self.nodes[i]) ):
                 currentNode = self.nodes[i][j]
-
-                dAbove = self.nodes[i-1][j] if i !=0 else None 
+                dAbove = self.nodes[i-1][j]     if i !=0 else None 
                 lAbove = self.nodes[i-1][j - 1] if i != 0 and j != 0 else None 
-                rAbove = self.nodes[i-1][j + 1] if i != 0 and j != 15 else None 
-                lMid = self.nodes[i][j-1] if j != 0 else None
-                rMid = self.nodes[i][j+1] if j != 15 else None
-                lBottom = self.nodes[i+1][j-1] if i != 15 and j != 0 else None
-                dBotttom = self.nodes[i+1][j] if i != 15 else None 
-                rBottom = self.nodes[i+1][j+1] if i != 15 and j != 15 else None
+                rAbove = self.nodes[i-1][j + 1] if i != 0 and j != maxJ else None 
+                lMid = self.nodes[i][j-1]       if j != 0 else None
+                rMid = self.nodes[i][j+1]       if j != maxJ else None
+                lBottom = self.nodes[i+1][j-1]  if i != maxI and j != 0 else None
+                dBotttom = self.nodes[i+1][j]   if i != maxI else None 
+                rBottom = self.nodes[i+1][j+1]  if i != maxI and j != maxJ else None
 
                 neighborListToFilter = [dAbove, lAbove, rAbove, lMid, rMid, lBottom, dBotttom, rBottom]
                 neighborList = [x for x in neighborListToFilter if x != None ]
